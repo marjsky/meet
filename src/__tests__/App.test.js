9a2ee25ceab6a6d225f73a086cfe70f7502851b2
marjsky <67877013+marjsky@users.noreply.github.com>
2022-08-27 18:59:59 -0400

    test app

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+import EventList from '../EventList';
+import CitySearch from '../CitySearch';
+
+describe('<App /> component', () => {
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App/>);
+  });
+
+  test('render list of events', () => {
+    expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
+
+  //SCENARIO 2: USER SHOULD SEE A LIST OF SUGGESTIONS WHEN T HEY SEARCH FOR A CITY
+  test('render CitySearch', () => {
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
+});
