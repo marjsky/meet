d39592434a70106363e5a451c14dc3230ed57d46
marjsky <67877013+marjsky@users.noreply.github.com>
2022-08-28 14:02:59 -0400

    revised codebase

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import Event from './Event';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CitySearch />
-        <EventList />
+        <EventList />        
       </div>
     );
   }
