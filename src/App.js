bcc66cc2f5d4bba43905e3df123a98fcf2d6ba23
marjsky <67877013+marjsky@users.noreply.github.com>
2022-08-31 06:26:40 -0400

    add numberOfEvents component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import Event from './Event';
+import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CitySearch />
+        <NumberOfEvents />
         <EventList />        
       </div>
     );
