3f541ee171070b1482f324b33691f17d1b49b1ca
marjsky <67877013+marjsky@users.noreply.github.com>
2022-08-31 16:53:02 -0400

    add test change state input

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -3,9 +3,17 @@ import React, { Component } from "react";
 class NumberOfEvents extends Component {
   constructor() {
     super();
-    this.state = { countEvents: 32 }
+    this.state = { countEvents: 32, errorText: '' }
   };
 
+  handleInputChange = (event) => {
+    const value = event.target.value;
+    if ((value < 1) || (value > 33)) {
+      this.setState({ errorText: 'Limit range  from 1 to 32'});
+    } else {
+      this.setState({ countEvents: value});
+    }
+  }
   render () {
     return (
       <div className="numberOfEvents">
@@ -15,7 +23,7 @@ class NumberOfEvents extends Component {
             type='number'
             className='input-number'
             value={this.state.countEvents}
-            
+            onChange={this.handleInputChange}
             />
         </label>
       </div>
