fb9857788afcfc89a759c9ad0d7015bc11c3fa03
marjsky <67877013+marjsky@users.noreply.github.com>
2022-09-02 20:25:04 -0400

    test select see all cities

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import Event from './Event';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents } from './api';
+import { extractLocations, getEvents } from './api';
 
 class App extends Component {
   state = {
@@ -12,13 +12,28 @@ class App extends Component {
     locations: []
   }
 
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
   updateEvents = (location) => {
     getEvents().then((events) => {
-      const locationEvents = events.filter((event) => event.location === location);
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
       this.setState({
         events: locationEvents
       });
     });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
   }
 
   render() {
