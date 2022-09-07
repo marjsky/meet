ba762d4e1250ceae4aae8e4ff306891ba6f58df5
marjsky <67877013+marjsky@users.noreply.github.com>
2022-09-07 02:30:38 -0400

    add testing  event number input

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -6,7 +6,7 @@ import NumberOfEvents from '../NumberOfEvents';
 describe('<NumberOfEvents /> component', () => {
   let NumberOfEventsWrapper;
   beforeAll(() => {
-    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+    NumberOfEventsWrapper = shallow(<NumberOfEvents updateEvents={() => {}} />);
   });
 
   test('component render', () => {
@@ -25,10 +25,24 @@ describe('<NumberOfEvents /> component', () => {
     expect(NumberOfEventsWrapper.state('countEvents')).toBe(32);
   });
 
+  test('display 32 by default', () => {
+    expect(
+        NumberOfEventsWrapper.find('.input-number').get(0).props.value
+    ).toEqual(32);
+});
+
+  test('user input is not in range 1-32', () => {
+    NumberOfEventsWrapper.find('.input-number').simulate(
+        'change', {target: { value: 40 }}
+    );
+    expect(NumberOfEventsWrapper.state('countEvents')).toEqual(32);
+});
+
   test('render change state of events number input', () => {
     NumberOfEventsWrapper.setState({ countEvents: 32});
     const eventObject = { target: {value: 7 }};
     NumberOfEventsWrapper.find('.input-number').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('countEvents')).not.toEqual(undefined);
     expect(NumberOfEventsWrapper.state('countEvents')).toBe(7);
   });
   
