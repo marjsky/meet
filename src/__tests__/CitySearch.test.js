5fd8168ff9b7e7c0776f55b3b549ec4c3c603cef
marjsky <67877013+marjsky@users.noreply.github.com>
2022-09-07 02:42:52 -0400

    clean codebase

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -12,23 +12,23 @@ describe('<CitySearch /> component', () => {
   });
 
   test('render text input', () => {
-    // const CitySearchWrapper = shallow(<CitySearch />);
+   
     expect(CitySearchWrapper.find('.city')).toHaveLength(1);
   });
 
   test('renders a list of sugggestions', () => {
-    // const CitySearchWrapper = shallow(<CitySearch />);
+   
     expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
   });
 
   test('renders text input correctly', () => {
-    // const CitySearchWrapper = shallow(<CitySearch />);
+   
     const query = CitySearchWrapper.state('query');
     expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
   });
 
   test('change state when text input changes', () => {
-    // const CitySearchWrapper = shallow(<CitySearch />);
+   
     CitySearchWrapper.setState({
       query: 'Munich'
     });
@@ -39,7 +39,7 @@ describe('<CitySearch /> component', () => {
   
   test('render list of suggestions correctly', () => {
     const locations = extractLocations(mockData);
-    // const CitySearchWrapper = shallow(<CitySearch />);
+   
     CitySearchWrapper.setState({ suggestions: locations });
     const suggestions = CitySearchWrapper.state('suggestions');
     expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(suggestions.length + 1);
