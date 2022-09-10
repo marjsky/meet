2eb90c36ff645851ba82ac6d7781b69435813417
marjsky <67877013+marjsky@users.noreply.github.com>
2022-09-09 20:24:24 -0400

    update codebase and correct

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,5 +1,3 @@
-
-import { getJestCucumberConfiguration } from 'jest-cucumber/dist/src/configuration';
 import puppeteer from 'puppeteer';
 
 describe('show/hide an event details', () => {
@@ -9,10 +7,9 @@ describe('show/hide an event details', () => {
     jest.setTimeout(30000);
     browser = await puppeteer.launch({
       headless: false,
-      slowMo: 250, // slow down by 250ms
-      ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
+      slowMo: 250,
+      ignoreDefaultArgs: ['--disable-extensions']
     });
-    browser = await puppeteer.launch(); // by default headless is true
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('.event');
@@ -23,19 +20,30 @@ describe('show/hide an event details', () => {
   });
 
   test('An event element is collapsed by default', async () => {
-    const eventDetails = await page.$('.event .event-description');
-    expect(eventDetails).toBeNull();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+    const extra = await page.$('.event .event__Details');
+
+    expect(extra).toBeNull();
   });
 
   test('User can expand an event to see its details', async () => {
-    await page.click('.event .event-showDetails-btn');
-    const eventDetails = await page.$('.event .event-description');
-    expect(eventDetails).toBeDefined();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+    await page.click('.event .show-button');
+
+    const extra = await page.$('.event .extra');
+    expect(extra).toBeDefined();
   });
 
   test('User can collapse an event to hide its details', async () => {
-    await page.click('.event .event-hideDetails-btn');
-    const eventDetails = await page.$('.event .event-description');
-    expect(eventDetails).toBeNull();
+    await page.click('.event .show-button');
+
+    const extra = await page.$('.event .extra');
+    expect(extra).toBeNull();
   });
 });
