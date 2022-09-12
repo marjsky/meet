bea2b4e45262bf840dea4ff3e3251900a3e39559
marjsky <67877013+marjsky@users.noreply.github.com>
2022-09-12 02:48:00 -0400

    fix concatenace string

--- src/api.js
+++ src/api.js
@@ -22,7 +22,7 @@ const getToken = async (code) => {
   try {
     const encodeCode = encodeURIComponent(code);
 
-    const response = await fetch('https://y1xfyrc9n7.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode);
+    const response = await fetch(`https://y1xfyrc9n7.execute-api.eu-central-1.amazonaws.com/dev/api/token/${encodeCode}`);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`)
     }
@@ -70,7 +70,7 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = 'https://y1xfyrc9n7.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
+    const url = `https://y1xfyrc9n7.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/${token}`;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
